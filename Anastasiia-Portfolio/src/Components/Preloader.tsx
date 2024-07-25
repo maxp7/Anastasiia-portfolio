1ff6e481d27a249ef7efadcea6cc12a35c5bddde
Max <podlughniy.maxim@gmail.com>
2024-07-25 13:38:19 +0200

    Cursor position on preloader

--- Anastasiia-Portfolio/src/Components/Preloader.tsx
+++ Anastasiia-Portfolio/src/Components/Preloader.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../Styles/Preloader.css';
 import useSound from 'use-sound';
 import sound from '../backgroundSound.wav';
@@ -9,38 +9,41 @@ interface PreloaderProps {
 }
 
 const Preloader: React.FC<PreloaderProps> = ({ onExploreClick }) => {
-  // Настроим параметр loop на true
   const [play, { stop }] = useSound(sound, { loop: true });
-  const [isVisible, setIsVisible] = useState(true); // Состояние видимости preloader
-  const [shouldAnimate, setShouldAnimate] = useState(false); // Состояние начала анимации
-  const [buttonVisible, setButtonVisible] = useState(true); // Состояние видимости кнопки
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 }); // Состояние для хранения координат курсора
+  const [isVisible, setIsVisible] = useState(true);
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+  const [buttonVisible, setButtonVisible] = useState(true);
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [buttonSize, setButtonSize] = useState({ width: 0, height: 0 });
 
   useEffect(() => {
-    // Обработчик для отслеживания положения курсора
     const handleMouseMove = (event: MouseEvent) => {
-      setCursorPosition({ x: event.clientX - 100, y: event.clientY - 110 });
+      setCursorPosition({ x: event.clientX, y: event.clientY });
     };
 
-    // Добавляем слушатель события mousemove
     window.addEventListener('mousemove', handleMouseMove);
-
-    // Удаляем слушатель события при размонтировании компонента
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     };
   }, []);
 
+  useEffect(() => {
+    if (buttonRef.current) {
+      const { offsetWidth, offsetHeight } = buttonRef.current;
+      setButtonSize({ width: offsetWidth, height: offsetHeight });
+    }
+  }, [buttonVisible]);
+
   const handleClick = () => {
     play();
     onExploreClick();
-    setButtonVisible(false); // Скрыть кнопку немедленно
-    setShouldAnimate(true); // Запустить анимацию
-
-    // Устанавливаем видимость preloader в false после завершения анимации
+    setButtonVisible(false);
+    setShouldAnimate(true);
     setTimeout(() => {
       setIsVisible(false);
-    }, 1); // Длительность анимации
+    }, 1);
   };
 
   return (
@@ -49,15 +52,20 @@ const Preloader: React.FC<PreloaderProps> = ({ onExploreClick }) => {
         <motion.div
           className="preloader"
           initial={{ opacity: 1 }}
-          animate={shouldAnimate ? { opacity: 0 } : { opacity: 1 }} // Запускаем анимацию
+          animate={shouldAnimate ? { opacity: 0 } : { opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 2 }} // Длительность анимации
+          transition={{ duration: 2 }}
         >
           {buttonVisible && (
             <button
+              ref={buttonRef}
               className="button"
               onClick={handleClick}
-              style={{ position: 'absolute', left: cursorPosition.x, top: cursorPosition.y }}
+              style={{
+                position: 'absolute',
+                left: cursorPosition.x - buttonSize.width / 2,
+                top: cursorPosition.y - buttonSize.height / 2,
+              }}
             >
               Explore
             </button>
